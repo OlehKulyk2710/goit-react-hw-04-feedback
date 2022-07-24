5d3c4128c7c3445be8671b35610f640df916473a
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-07-24 15:16:59 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,67 +1,131 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Statistics from 'components/Statistics/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Section from 'components/Section/Section';
 import Notification from 'components/Notification/Notification';
 import css from './App.module.css';
+// import { useState } from 'react';
+import { useReducer } from 'react';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+// ------ Option #1: using useReducer ------
 
-  handleClick = event => {
+const handleReducer = (state, action) => {
+  const { type, payload } = action;
+
+  return { ...state, [type]: state[type] + payload };
+};
+
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(handleReducer, initialState);
+  const { good, neutral, bad } = state;
+
+  const handleClick = event => {
     const normalizedTextContent = event.target.textContent.toLowerCase();
-    this.setState(prevState => ({
-      [normalizedTextContent]: prevState[normalizedTextContent] + 1,
-    }));
+    dispatch({ type: normalizedTextContent, payload: 1 });
   };
 
-  countTotalFeedback = () => {
-    const feedbacksValues = Object.values(this.state);
-
-    return feedbacksValues.reduce((prev, value) => prev + value, 0);
+  const countTotalFeedbacks = () => {
+    return Object.values(state).reduce((acc, item) => acc + item, 0);
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const totalFeedbacks = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedbacks = countTotalFeedbacks();
     const posFeedbPerc = Math.round((good / totalFeedbacks) * 100);
     return posFeedbPerc ? posFeedbPerc : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const buttonsNames = Object.keys(this.state);
+  const buttonsNames = Object.keys(state);
+  return (
+    <div className={css.container}>
+      <h1 className={css.header__title}>Cafe Espresso (feedbacks)</h1>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={buttonsNames} onLeaveFeedback={handleClick} />
+      </Section>
 
-    return (
-      <div className={css.container}>
-        <h1 className={css.header__title}>Cafe Espresso (feedbacks)</h1>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={buttonsNames}
-            onLeaveFeedback={this.handleClick}
+      <Section title="Statistics">
+        {countTotalFeedbacks() === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedbacks()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default App;
+
+// ------ Option #2: using useState ------
+
+// const App = () => {
+//   const [good, setGood] = useState(0);
+//   const [neutral, setNeutral] = useState(0);
+//   const [bad, setBad] = useState(0);
+
+//   const handleClick = event => {
+//     const normalizedTextContent = event.target.textContent.toLowerCase();
+
+//     switch (normalizedTextContent) {
+//       case 'good':
+//         setGood(good + 1);
+//         break;
+//       case 'neutral':
+//         setNeutral(neutral + 1);
+//         break;
+//       case 'bad':
+//         setBad(bad + 1);
+//         break;
+//       default:
+//         console.log('Unrecognized value:', normalizedTextContent);
+//     }
+//   };
+
+//   const countTotalFeedback = () => {
+//     return good + neutral + bad;
+//   };
+
+//   const countPositiveFeedbackPercentage = () => {
+//     const totalFeedbacks = countTotalFeedback();
+//     const posFeedbPerc = Math.round((good / totalFeedbacks) * 100);
+//     return posFeedbPerc ? posFeedbPerc : 0;
+//   };
+
+//   const buttonsNames = ['good', 'neutral', 'bad'];
+
+//   return (
+//     <div className={css.container}>
+//       <h1 className={css.header__title}>Cafe Espresso (feedbacks)</h1>
+//       <Section title="Please leave feedback">
+//         <FeedbackOptions options={buttonsNames} onLeaveFeedback={handleClick} />
+//       </Section>
+
+//       <Section title="Statistics">
+//         {countTotalFeedback() === 0 ? (
+//           <Notification message="There is no feedback" />
+//         ) : (
+//           <Statistics
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={countTotalFeedback()}
+//             positivePercentage={countPositiveFeedbackPercentage()}
+//           />
+//         )}
+//       </Section>
+//     </div>
+//   );
+// };
+
+// export default App;
